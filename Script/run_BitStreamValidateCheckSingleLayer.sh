e455d9078691baa7a2fd6800e85b90910d8e7942
Forrest Shi <shihuadesysu@gmail.com>
2014-07-04 16:59:37 +0800

    Update run_BitStreamValidateCheckSingleLayer.sh

--- Script/run_BitStreamValidateCheckSingleLayer.sh
+++ Script/run_BitStreamValidateCheckSingleLayer.sh
@@ -61,7 +61,7 @@ runWelsDecCheck()
   echo ".....................WelsDecoder log...................">>${CheckLogFile}
   echo "">>${CheckLogFile}
   ./h264dec     ${BitStreamFile}   ${DecYUVFile} 2>>${CheckLogFile}
-    let "WelsDecodeFlag=$?"
+  let "WelsDecodeFlag=$?"
   if [ ! ${WelsDecodeFlag}  -eq 0  ]
   then
     return 1
