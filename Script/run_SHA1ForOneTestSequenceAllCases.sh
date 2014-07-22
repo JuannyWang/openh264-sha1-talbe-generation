8f7c1629acfbb74a970e5fbbe0bebfd518de8a0e
shihuade <shihuadesysu@gmail.com>
2014-07-22 05:56:26 -0400

    update SHA1 script

--- Script/run_SHA1ForOneTestSequenceAllCases.sh
+++ Script/run_SHA1ForOneTestSequenceAllCases.sh
@@ -21,14 +21,10 @@
 #            ${AllCaseConsoleLogFile}
 #            ${CaseSummaryFile}
 #
-#
-#
 #date:  10/06/2014 Created
 #***************************************************************************************
 runGlobalVariableInitial()
 {
-    #TestDataSpaceDir=../AllTestData  CurrentDir=../AllTestData/TestSetXXX/***.yuv   eg ../AllTestData/TestSetCIF/basketball.yuv
-  #WorkingDir folder include   ./AllTestData   ./result  ./bats  ./cfg
   CurrentDir=`pwd`
   #test data space
   FinalResultPath="result"
@@ -38,59 +34,40 @@ runGlobalVariableInitial()
   mkdir -p ${IssueDataPath}
   mkdir -p ${TempDataPath}
   
-  #get YUV detail info $picW $picH $FPS
-  declare -a aYUVInfo
-  aYUVInfo=(`./run_ParseYUVInfo.sh  ${TestSequenceName}`)
-  PicW=${aYUVInfo[0]}
-  PicH=${aYUVInfo[1]}
-  FPS=${aYUVInfo[2]}
-  
-  if [  ${PicW} -eq 0 -o ${PicH} -eq 0  ]
-  then
-	echo "YUVName is not correct,should be named as ABC_PicWXPicH_FPS.yuv"
-	exit 1
-  fi
-  
-  if [  ${FPS} -eq 0  ]
-  then
-    let "FPS=10"
-  fi
-   
-  if [  ${FPS} -gt 50  ]
-  then
-    let "FPS=50"
-  fi
-  
   #test cfg file and test info output file
   ConfigureFile=welsenc.cfg
   AllCasePassStatusFile="${FinalResultPath}/${TestSequenceName}_AllCaseOutput.csv"
   AllCaseSHATableFile="${FinalResultPath}/${TestSequenceName}_AllCase_SHA1_Table.csv"
   AllCaseConsoleLogFile="${FinalResultPath}/${TestSequenceName}.TestLog"
   CaseSummaryFile="${FinalResultPath}/${TestSequenceName}.Summary"
-  echo  "EncoderFlag,DecoderFlag,SHA-1 Value,\
-        MD5String, BitStreamSize, YUVSize,   \
-        -frms, -numtl, -scrsig, -rc,         \
-        -tarb, -lqp 0, -iper,                \
-        -slcmd 0,-slcnum 0, -thread,         \
-        -ltr, -db, -MaxNalSize, -denois,     \
-        -scene,  -bgd,-aq">${AllCasePassStatusFile}
-  echo  "SHA-1 Value,                      \
-        MD5String, BitStreamSize, YUVSize, \
-        -frms, -numtl, -scrsig, -rc,       \
-        -tarb, -lqp 0, -iper,              \
-        -slcmd 0,-slcnum 0, -thread,       \
-        -ltr, -db, -MaxNalSize,-denois,    \
-        -scene,  -bgd, -aq">${AllCaseSHATableFile}
-  #intial Commandline parameters
+  echo  "EncoderFlag,DecoderFlag,SHA-1, MD5, BitStreamSize, YUVSize,\
+        -scrsig,  -frms,  -numl,  -numtl,-sh, -sw,\
+		-dw 0, -dh 0, -dw 1, -dh 1,-dw 2, -dh 2, -dw 3, -dh 3,\
+		-frout 0,  -frout 1, -frout 2, -frout 3,\
+		-lqp 0, -lqp 1, -lqp 2, -lqp 3,\
+		-rc,-tarb, -ltarb 0, -ltarb 1, -ltarb 2, -ltarb 3,\ 
+		-nalsize, -iper, -thread, -ltr, -db, -denois,\ 
+		-scene    -bgd    -aq ">${AllCasePassStatusFile}
+  echo  "SHA-1 Value, MD5String, BitStreamSize, YUVSize, \
+        -scrsig,  -frms,  -numl,  -numtl,-sh, -sw,\
+		-dw 0, -dh 0, -dw 1, -dh 1,-dw 2, -dh 2, -dw 3, -dh 3,\
+		-frout 0,  -frout 1, -frout 2, -frout 3,\
+		-lqp 0, -lqp 1, -lqp 2, -lqp 3,\
+		-rc,-tarb, -ltarb 0, -ltarb 1, -ltarb 2, -ltarb 3,\ 
+		-nalsize, -iper, -thread, -ltr, -db, -denois,\ 
+		-scene    -bgd    -aq ">${AllCaseSHATableFile}
+		
+  #initial command line parameters
   declare -a EncoderCommandSet
   declare -a EncoderCommandName
   declare -a EncoderCommandValue
   #encoder parameters  change based on the case info
   CaseInfo=""
   BitStreamFile=""
-  RecYUVFile=""
-  JMDecFile=""
-  DiffInfo=""
+  RecYUVFileLayer0=""
+  RecYUVFileLayer1=""
+  RecYUVFileLayer2=""
+  RecYUVFileLayer3=""
   DiffFlag=""
   EncoderCommand=""
   EncoderLog="encoder.log"
@@ -110,42 +87,30 @@ runGlobalVariableInitial()
 #usage runEncoderCommandInital
 runEncoderCommandInital()
 {
-  EncoderCommandSet=(-frms  \
-                -numtl      \
-                -scrsig     \
-                -rc         \
-                -tarb       \
-                "-lqp 0"    \
-                -iper       \
-                "-slcmd 0"  \
-                "-slcnum 0" \
-                -thread     \
-                -ltr        \
-                -db         \
-                "-nalsize " \
-                -denois     \
-                -scene      \
-                -bgd        \
-                -aq)
-  EncoderCommandName=(FrEcoded \
-                NumTempLayer   \
-                ContentSig     \
-                RC             \
-                BitRate        \
-                QP             \
-                IntraPeriod    \
-                SlcMd          \
-                SlcMum         \
-                ThrMum         \
-                LTR            \
-                LFilterIDC     \
-                MaxNalSize     \
-                DenoiseFlag    \
-                SceneChangeFlag \
-                BackgroundFlag  \
-                AQFlag)
-  EncoderCommandValue=(0 0 0 0 0     0 0 0 0 0     0 0 0 0 0   0  0)
+  EncoderCommandSet=(-scrsig  -frms  -numl   -numtl \
+				-sh -sw  "-dw 0"  "-dh 0" "-dw 1" "-dh 1" "-dw 2" "-dh 2" "-dw 3" "-dh 3" \
+				"-frout 0" "-frout 1" "-frout 2" "-frout 3" \
+				"-lqp 0" "-lqp 1" "-lqp 2" "-lqp 3" \
+				-rc -tarb "-ltarb 0" 	"-ltarb 1" "-ltarb 2" "-ltarb 3" \
+				"-slcmd 0" "-slcnum 0" "-slcmd 1" "-slcnum 1"\
+				"-slcmd 2" "-slcnum 2" "-slcmd 3" "-slcnum 3"\
+				-nalsize -iper   -thread    -ltr \
+				-db  -denois    -scene    -bgd    -aq )
+  EncoderCommandName=(scrsig  frms  numl   numtl \
+				sw sh  dw0 dh0 dw1 dh1 dw2 dh2 dw3 dh3 \
+				frout0 frout1 frout2 frout3 \
+				lqp0 lqp1 lqp2 lqp3 \
+				rc tarb ltarb0 	ltarb1 ltarb2 ltarb3 \
+				slcmd0 slcnum0 slcmd1 slcnum1 \
+				slcmd2 slcnum2 slcmd3 slcnum3 \
+				nalsize iper   thread  ltr \
+				db  denois  scene  bgd  aq )	
   NumParameter=${#EncoderCommandSet[@]}
+  
+  for ((i=0;i<NumParameter; i++))
+  do
+		EncoderCommandValue[$i]=0
+  done
 }
 #***********************************************************
 #call by  runAllCaseTest
@@ -161,58 +126,67 @@ runParseCaseInfo()
     local TempData=""
   local BitstreamPrefix=""
   local CaseData=$@
+  declare -a aTempParamIndex=( 6 7 8 9 10 11 12 13    15 16 17   19 20 21     24 25 26 27   30 31 32 33 34 35  )
   TempData=`echo $CaseData |awk 'BEGIN {FS="[,\r]"} {for(i=1;i<=NF;i++) printf(" %s",$i)} ' `
   EncoderCommandValue=(${TempData})
+  
+  let "TempParamFlag=0"
   for((i=0; i<$NumParameter; i++))
   do
-    BitstreamPrefix=${BitstreamPrefix}_${EncoderCommandName[$i]}_${EncoderCommandValue[$i]}
+	for ParnmIndex in ${aTempParamIndex[@]}
+	do
+	  if [  $i -eq ${ParnmIndex} ]
+	  then
+		let "TempParamFlag=1"
+	  fi
+	done
+	
+	if [ ${TempParamFlag} -eq 0 ]
+	then
+	  BitstreamPrefix=${BitstreamPrefix}_${EncoderCommandName[$i]}_${EncoderCommandValue[$i]}
+	fi
+	let "TempParamFlag=0"    
   done
-  BitstreamTarget=${TempDataPath}/${TestSequenceName}_${BitstreamPrefix}_codec_target.264
-  RecYUVFile=${TempDataPath}/${TestSequenceName}_${BitstreamPrefix}_rec.yuv
-  DecYUVFile=${TempDataPath}/${TestSequenceName}_${BitstreamPrefix}_dec.yuv
-  JMDecYUVFile=${TempDataPath}/${TestSequenceName}_${BitstreamPrefix}_JM_Dec.yuv
-  DiffInfo=${TempDataPath}/${TestSequenceName}_${BitstreamPrefix}_diff.info
-  echo ""
-  echo "BitstreamPrefix is ${BitstreamPrefix}"
-  echo ""
+  
+  BitStreamFile=${TempDataPath}/${TestSequenceName}_${BitstreamPrefix}_codec_target.264
+  
+  RecYUVFileLayer0=${TempDataPath}/${TestSequenceName}_rec0.yuv
+  RecYUVFileLayer1=${TempDataPath}/${TestSequenceName}_rec1.yuv
+  RecYUVFileLayer2=${TempDataPath}/${TestSequenceName}_rec2.yuv
+  RecYUVFileLayer3=${TempDataPath}/${TestSequenceName}_rec3.yuv
+  
 }
 #call by  runAllCaseTest
 #usage  runEncodeOneCase
 runEncodeOneCase()
 {
-  BitStreamFile=${BitstreamTarget}
-  CaseCommand="${ConfigureFile}  \
-    -numl 1                      \
-    -lconfig 0 layer2.cfg        \
-    -sw   ${PicW} -sh   ${PicH}  \
-    -dw 0 ${PicW} -dh 0 ${PicH}  \
-    -frout 0  ${FPS}             \
-    -ltarb 0  ${EncoderCommandValue[4]} \
-    ${EncoderCommandSet[0]}  ${EncoderCommandValue[0]}  \
-    ${EncoderCommandSet[1]}  ${EncoderCommandValue[1]}  \
-    ${EncoderCommandSet[2]}  ${EncoderCommandValue[2]}  \
-    ${EncoderCommandSet[3]}  ${EncoderCommandValue[3]}  \
-    ${EncoderCommandSet[4]}  ${EncoderCommandValue[4]}  \
-    ${EncoderCommandSet[5]}  ${EncoderCommandValue[5]}  \
-    ${EncoderCommandSet[6]}  ${EncoderCommandValue[6]}  \
-    ${EncoderCommandSet[7]}  ${EncoderCommandValue[7]}  \
-    ${EncoderCommandSet[8]}  ${EncoderCommandValue[8]}  \
-    ${EncoderCommandSet[9]}  ${EncoderCommandValue[9]}  \
-    ${EncoderCommandSet[10]} ${EncoderCommandValue[10]} \
-    ${EncoderCommandSet[11]} ${EncoderCommandValue[11]} \
-    ${EncoderCommandSet[12]} ${EncoderCommandValue[12]} \
-    ${EncoderCommandSet[13]} ${EncoderCommandValue[13]} \
-    ${EncoderCommandSet[14]} ${EncoderCommandValue[14]} \
-    ${EncoderCommandSet[15]} ${EncoderCommandValue[15]} \
-    ${EncoderCommandSet[16]} ${EncoderCommandValue[16]}"
-  echo ""
-  echo "case line is :"
-  EncoderCommand="./h264enc  ${CaseCommand}  -bf   ${BitStreamFile}  -org   ${InputYUV}  -drec 0 ${RecYUVFile}"
-  echo ${EncoderCommand}
-  ./h264enc ${CaseCommand}   \
-    -bf     ${BitStreamFile} \
-    -org    ${InputYUV}      \
-    -drec 0 ${RecYUVFile} >${EncoderLog}
+ 
+	local TempCammand=""
+	for ((i=3; i<${NumParameter}; i++))
+	do
+		TempCammand="${TempCammand} ${EncoderCommandSet[$i]}  ${EncoderCommandValue[$i]} " 
+	done
+	TempCammand="${EncoderCommandSet[0]} ${EncoderCommandValue[0]} \
+				${EncoderCommandSet[1]}  ${EncoderCommandValue[1]} \
+				${EncoderCommandSet[2]}  ${EncoderCommandValue[2]} \
+				-lconfig 0 layer0.cfg \
+				-lconfig 1 layer1.cfg \
+				-lconfig 2 layer2.cfg \
+				-lconfig 3 layer3.cfg \
+				${TempCammand}"
+	echo ""
+	echo "case line is :"
+	EncoderCommand="./h264enc  ${TempCammand}  -bf   ${BitStreamFile}  -org   ${InputYUV} \
+					-drec 0 ${RecYUVFileLayer0} \
+					-drec 1 ${RecYUVFileLayer1} \
+					-drec 2 ${RecYUVFileLayer2} \
+					-drec 3 ${RecYUVFileLayer3}"
+	echo ${EncoderCommand}
+	./h264enc ${TempCammand}   -bf     ${BitStreamFile} -org    ${InputYUV} \
+		-drec 0 ${RecYUVFileLayer0} \
+		-drec 1 ${RecYUVFileLayer1} \
+		-drec 2 ${RecYUVFileLayer2} \
+		-drec 3 ${RecYUVFileLayer3}>${EncoderLog}
 }
 #usage: runGetFileSize  $FileName
 runGetFileSize()
@@ -382,9 +356,9 @@ runSingleCasePostAction()
   CaseInfo=`echo $CaseData | awk 'BEGIN {FS="[,\r]"} {for(i=1;i<=NF;i++) printf(" %s,",$i)} '`
   if [ ${EncoderPassedFlag}  -eq  0  ]
   then
-    SHA1String=`sha1sum  -b  ${BitStreamFile}`
+    SHA1String=`openssl sha1   ${BitStreamFile}`
     SHA1String=`echo ${SHA1String} | awk '{print $1}' `
-    MD5String=`md5sum -b  ${BitStreamFile}`
+    MD5String=`openssl  md5  ${BitStreamFile}`
     MD5String=`echo ${MD5String} | awk '{print $1}' `
     YUVSize=`runGetFileSize  ${InputYUV}`
     BitStreamSize=`runGetFileSize  ${BitStreamFile}`
@@ -430,12 +404,12 @@ runAllCaseTest()
       #bit stream file validation checking,
       #encoder: Rec.yuv should be the same with JM_Dec.yuv
       #decoder: Rec.yuv should be the same with JM_Dec.yuv
-      CaseCheckResult=`./run_BitStreamValidateCheckSingleLayer.sh  ${BitStreamFile}  ${JMDecYUVFile}  ${DecYUVFile}  ${RecYUVFile} ${IssueDataPath}  ${CheckLogFile}`
+      #CaseCheckResult=`./run_BitStreamValidateCheckSingleLayer.sh  ${BitStreamFile}  ${JMDecYUVFile}  ${DecYUVFile}  ${RecYUVFile} ${IssueDataPath}  ${CheckLogFile}`
       echo ".........result parse.........${EncoderFailedFlag}   $CaseCheckResult"
-      runParseCheckResult  ${EncoderFailedFlag}   $CaseCheckResult
+      #runParseCheckResult  ${EncoderFailedFlag}   $CaseCheckResult
       cat ${CheckLogFile}
       echo "return value for bit stream is  ${CaseCheckResult}"
-      runSingleCasePostAction  ${CaseData}
+      #runSingleCasePostAction  ${CaseData}
       let "TotalCaseNum++"
     fi
   done <$AllCaseFile
